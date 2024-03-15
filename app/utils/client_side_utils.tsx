21e2e77df7548cedd77e614e1bfa6d24dc7e69ac
unknown <kalossi@gmail.com>
2024-03-15 17:08:16 +0200

    more spreading

---
+++ app/utils/client_side_utils.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { useEffect, useState } from "react";
+import { ServerSideInstances, fetchEc2Instances } from "../utils/server_side_utils";
+
+
+export const useServerSideEffect = () => {
+  const [serverSideInstances, setServerSideInstances] = useState<ServerSideInstances[]>([]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchEc2Instances();
+        setServerSideInstances(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } 
+    };
+  
+    fetchData();
+  }, []);
+
+  return serverSideInstances;
+};
