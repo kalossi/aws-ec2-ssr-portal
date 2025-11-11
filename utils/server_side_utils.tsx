588e1de05d9d4573cf9dd54b456010b94bc444fe
unknown <kalossi@gmail.com>
2025-11-11 08:21:19 +0200

    fixed dynamic ws

--- utils/server_side_utils.tsx
+++ utils/server_side_utils.tsx
@@ -1,5 +1,5 @@
 //.utils/server_side_utils.tsx
-import AWS from 'aws-sdk';
+import * as AWS from 'aws-sdk';
 //in server side you need to import these
 import { WebSocketServer, WebSocket } from 'ws';
 
@@ -10,8 +10,7 @@ export interface InitialServerSideInstance {
   privateIP: string | "N/A";
 }
 //invoke instance so that you can use it in many places
-const wss = new WebSocketServer({port: 8081});
-
+const wss = new WebSocketServer({port: 8085});
 // keep last broadcast payload to avoid sending unchanged data
 let lastBroadcastPayload = '';
 
@@ -23,6 +22,7 @@ const startWSServer = () => {
     //sent as string
     const message = JSON.stringify(instances);
     ws.send(message);
+
     // update lastBroadcastPayload so subsequent broadcasts know current state
     lastBroadcastPayload = message;
 
@@ -40,8 +40,10 @@ const broadcastUpdates = async () => {
   const instances = await fetchEc2Instances();
   //again, as string
   const message = JSON.stringify(instances);
+
   // only broadcast when something changed
   if (message === lastBroadcastPayload) return;
+
   lastBroadcastPayload = message;
   //array of clients, objects
   wss.clients.forEach(client => {
@@ -69,6 +71,15 @@ export const fetchEc2Instances = async () => {
     // console.log("fetching promise...");
     const data = await ec2.describeInstances().promise();
     // console.log("fetched promise...");
+
+    // ADDED: log summary of raw response so you can see why instances array is empty
+    // const reservations = data.Reservations || [];
+    // console.log(`describeInstances returned ${reservations.length} reservation(s).`);
+    // reservations.forEach((res, idx) => {
+    //   const ids = (res.Instances || []).map(i => ({ id: i.InstanceId, state: i.State?.Name, region: i.Placement?.AvailabilityZone }));
+    //   console.log(` reservation[${idx}] instances:`, ids);
+    // });
+
     const instances: InitialServerSideInstance[] = data.Reservations?.flatMap(
       (reservation) => {
         return reservation.Instances?.map((instance) => ({
